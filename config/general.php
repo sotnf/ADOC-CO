97c53f827750e743197155bf606ed51f01b69527
Jared Budlong <jared@sotnf.com>
2023-06-02 23:21:16 -0500

    Updates to make multi-site add new sections like social media and links.

--- config/general.php
+++ config/general.php
@@ -14,18 +14,29 @@ use craft\helpers\App;
 return GeneralConfig::create()
     // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
+    
     // Prevent generated URLs from including "index.php"
     ->omitScriptNameInUrls()
+    
     // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-    ->devMode(App::env('DEV_MODE') ?? false)
+    // ->devMode(App::env('DEV_MODE') ?? false)
+    // ->devMode(false)
+    
     // Preload Single entries as Twig variables
     ->preloadSingles()
+    
     // Allow administrative changes
     ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
+    
     // Disallow robots
     ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
     ->aliases([    
-        '@primarySiteUrl' => getenv('PRIMARY_SITE_URL'),
+        '@PrimarySiteUrl' => getenv('PRIMARY_SITE_URL'),
+        '@web' => App::env('PRIMARY_SITE_URL'),
+        '@webroot' => App::env('BASE_PATH'),
+        '@SOTNFSiteUrl' => getenv('SOTNF_SITE_URL'),
+        '@webADOC' => App::env('ADOC_SITE_URL'),
+        // '@webrootSOTNF' => App::env('BASE_PATH'),
         ])
 
 ;

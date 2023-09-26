adbc9b6afe9de62bb9a610247edd7aea17dbe3c1
Jared Budlong <jared@sotnf.com>
2023-09-26 00:41:21 -0500

    Fixing dev setup and htaccess

--- craft/config/general.php
+++ craft/config/general.php
@@ -15,31 +15,48 @@ return GeneralConfig::create()
     // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
     
-    // Prevent generated URLs from including "index.php"
-    ->omitScriptNameInUrls()
+    // // Prevent generated URLs from including "index.php"
+    // ->omitScriptNameInUrls(true)
     
-    // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-    // ->devMode(App::env('DEV_MODE') ?? false)
-    // ->devMode(false)
+    // // Allow upscaling of images
+    // ->upscaleImages(false)
+
+    // Enable GQL (GraphQL) queries
+    ->enableGql(true)
    
-    // Preload Single entries as Twig variables
-    ->preloadSingles()
+    // // Preload Single entries as Twig variables
+    // ->preloadSingles(true)
     
     // Allow administrative changes
     ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
     
     // Disallow robots
     ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
+    ->verificationCodeDuration(432000) // 5 days    
+    // ->errorTemplatePrefix('_')
+
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
+    ->defaultWeekStartDay(1)
+
+    
+
+    // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+    ->devMode(App::env('DEV_MODE') ?? false)
+    
+    
+
+    // Aliases
     ->aliases([    
-        '@PrimarySiteUrl' => getenv('PRIMARY_SITE_URL'),
         '@web' => App::env('PRIMARY_SITE_URL'),
         '@webroot' => App::env('BASE_PATH'),
-        '@SOTNFSiteUrl' => getenv('SOTNF_SITE_URL'),
-        '@webADOC' => App::env('ADOC_SITE_URL'),
+        // '@PrimarySiteUrl' => getenv('PRIMARY_SITE_URL'),
+        // '@SOTNFSiteUrl' => getenv('SOTNF_SITE_URL'),
+        // '@webADOC' => App::env('ADOC_SITE_URL'),
         // '@webrootSOTNF' => App::env('BASE_PATH'),
         '@mailGunAPI'=> getenv('MAILGUN_API'),
         ])
-    ->testToEmailAddress(['jbudlong@gmail.com' => 'Craft Test Email'])
+    // Email to use for ALL emails when not in production
+    ->testToEmailAddress(['jbudlong@gmail.com' => 'Craft Email (tk) Gen.php'])
 
 
 ;
